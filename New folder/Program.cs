20d4545d1b89fd4826800c4ff99916571dbee49e
Manoj Babu Yeluri <m.yeluri@zensar.com>
2022-06-24 11:42:11 +0530

    Sample

---
+++ New folder/Program.cs
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+﻿using System;
+
+namespace JavaScript_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+=======
+﻿using System;
+
+namespace JavaScript_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+>>>>>>> 96ffd256c7a5cd6aa08a817f19cfaa91ab2c3d80
